f84b93d471b096c49c0cc08022462cc1cc73c3e7
Ahmad Shaja AZIMI <ahmad.shojaazimi@gmail.com>
2023-09-11 13:02:47 +0300

    finished design + auth + routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,44 @@
-import React from "react";
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import Login from "./pages/Auth/Login";
-import Register from "./pages/Auth/Register";
-import Checkout from "./pages/Checkout";
-import IndexPage from "./pages/index/IndexPage";
-import Shop from "./pages/Shop";
+import preload from "./assets/walk.gif";
+
+function lazyWithTimeout(importFunction) {
+  return lazy(() =>
+    Promise.all([
+      importFunction(),
+      new Promise((resolve) => setTimeout(resolve, 600)),
+    ]).then(([moduleExports]) => moduleExports)
+  );
+}
+
+const LazyIndexPage = lazyWithTimeout(() => import("./pages/index/IndexPage"));
+const LazyShopPage = lazyWithTimeout(() => import("./pages/Shop"));
+const LazyCheckoutPage = lazyWithTimeout(() => import("./pages/Checkout"));
+const LazyLoginPage = lazyWithTimeout(() => import("./pages/auth/Login"));
+const LazyRegisterPage = lazyWithTimeout(() => import("./pages/auth/Register"));
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Header />}>
-        <Route index element={<IndexPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <>
+      <Header />
+      <Suspense
+        fallback={
+          <div className="h-screen flex justify-center items-center flex-col gap-5">
+            <img className="h-[70px] w-[70px]" src={preload} />
+            <p className="text-xl font-bold">Please wait....</p>
+          </div>
+        }
+      >
+        <Routes>
+          <Route index element={<LazyIndexPage />} />
+          <Route path="shop" element={<LazyShopPage />} />
+          <Route path="checkout" element={<LazyCheckoutPage />} />
+          <Route path="login" element={<LazyLoginPage />} />
+          <Route path="register" element={<LazyRegisterPage />} />
+        </Routes>
+      </Suspense>
+    </>
   );
 };
 
--- src/pages/index/IndexPage.jsx
+++ src/pages/index/IndexPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { categories } from "./categories.js";
-
+import { useNavigate } from "react-router-dom";
 const IndexPage = () => {
+  const navigate = useNavigate();
+  const goToShopHandler = () => {
+    navigate("/shop");
+  };
   return (
     <div className="grid lg:grid-cols-3  max-lg:grid-cols-1 gap-4 p-4 ">
       {categories.map(({ id, title, imageUrl, col }) => {
@@ -11,7 +15,10 @@ const IndexPage = () => {
             key={id}
             className={`category-container item-bg flex flex-col items-center justify-center ${col}  cursor-pointer items-center  min-h-[400px]  bg-cover bg-center bg-no-repeat duration-300`}
           >
-            <div className="category-body-container bg-gray-100 opacity-60 hover:opacity-100 duration-300 rounded-md p-4 border box-border border-black flex flex-col items-center gap-1">
+            <div
+              onClick={goToShopHandler}
+              className="category-body-container bg-gray-100 opacity-60 hover:opacity-100 duration-300 rounded-md p-4 border box-border border-black flex flex-col items-center gap-1"
+            >
               <h2 className="font-bold text-lg">{title}</h2>
               <p className="text-sm font-light ">Shop Now</p>
             </div>
